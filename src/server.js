29dfeb09a6eab963dfce5440a0315edae6e02fa0
Tina Landar <landartina@gmail.com>
2025-10-09 14:24:51 +0300

    first-commit

---
+++ src/server.js
@@ -0,0 +1,87 @@
+import 'dotenv/config';
+
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
+
+const app = express();
+
+const PORT = Number(getEnvVar('PORT', '3000'));
+
+export default function setupServer() {
+  try {
+    app.use(express.json());
+    app.use(cors());
+    app.use(
+      pino({
+        transport: {
+          target: 'pino-pretty',
+        },
+      }),
+    );
+    app.get('/contacts', async (req, res) => {
+      const contacts = await getAllContacts();
+      res.json({
+        status: 200,
+        message: 'Successfully found contacts!',
+        data: contacts,
+      });
+    });
+    app.get('/contacts/:contactId', async (req, res, next) => {
+      const { contactId } = req.params;
+      const contact = await getContactById(contactId);
+
+      if (!contact) {
+        res.status(404).json({
+          message: 'Contact not found',
+        });
+        return;
+      }
+
+      res.status(200).json({
+        data: contact,
+      });
+    });
+
+    app.use((req, res) => {
+      res.status(404).json({
+        message: 'Not found',
+      });
+    });
+    app.use((err, req, res, next) => {
+      res.status(500).json({
+        message: 'Something went wrong',
+        error: err.message,
+      });
+    });
+
+    app.listen(PORT, (error) => {
+      if (error) {
+        throw error;
+      }
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+//   app.get('/contacts/:contactId', async (req, res, next) => {
+//     const { contactId } = req.params;
+//     const contact = await getContactById(contactId);
+
+//     // Відповідь, якщо контакт не знайдено
+//     if (!contact) {
+//       res.status(404).json({
+//         message: 'Contact not found',
+//       });
+//       return;
+//     }
+
+//     // Відповідь, якщо контакт знайдено
+//     res.status(200).json({
+//       data: contact,
+//     });
+//   });
